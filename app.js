9a9176201956d8b2d4c7a9a5364b3894c8210c91
Harshit Dugar <68436118+harshit-dugar@users.noreply.github.com>
2023-05-06 12:42:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -193,8 +193,9 @@ app.post('/login',(req,res)=>{
     // })
 });
 
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000");
+app.listen(PORT,()=>{
+    console.log("Server started");
 });
